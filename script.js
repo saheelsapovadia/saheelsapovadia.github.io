f4e6041eaaf7fdabb1bf38117c2956605e31361b
Saheel Sapovadia <saheelsapovadia23@gmail.com>
2024-04-02 00:31:09 +0530

    v2 temp

--- script.js
+++ script.js
@@ -86,3 +86,28 @@ tabs.forEach((tab) => {
     target.classList.add("qualification__active");
   });
 });
+
+let swiperCards = new Swiper(".gallery-cards", {
+  loop: true,
+  loopedSlides: 5,
+  cssMode: true,
+  effect: "fade",
+});
+
+let swiperThumbs = new Swiper(".gallery-thumbs", {
+  loop: true,
+  loopedSlides: 5,
+  slidesPerView: 3,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+swiperThumbs.controller.control = swiperCards;
